4ea753b6c39232a2510e632049fcd41d44e18a8b
Chongyun Lee <uchkks@protonmail.com>
2024-01-11 01:14:38 +0800

    python-numpy: revbump to rebuild

--- packages/python-numpy/build.sh
+++ packages/python-numpy/build.sh
@@ -3,12 +3,13 @@ TERMUX_PKG_DESCRIPTION="The fundamental package for scientific computing with Py
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.26.3"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=git+https://github.com/numpy/numpy
 TERMUX_PKG_DEPENDS="libc++, libopenblas, python"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE="latest-release-tag"
-TERMUX_PKG_PYTHON_COMMON_DEPS="'setuptools==59.2.0', 'wheel==0.38.1', 'Cython>=0.29.34,<3.1'"
+TERMUX_PKG_PYTHON_COMMON_DEPS="'wheel==0.38.1', 'Cython>=0.29.34,<3.1'"
 TERMUX_PKG_RM_AFTER_INSTALL="
 bin/
 "
